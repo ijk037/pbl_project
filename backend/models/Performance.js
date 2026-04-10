efcb601294ddee5604b993e4c0a54ebba63dc619
ijk037 <mail.ishpreetkaur@gmail.com>
2026-04-10 19:03:23 +0530

    Removed old project files

--- backend/models/Performance.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const performanceSchema = new mongoose.Schema({
-  studentId: mongoose.Schema.Types.ObjectId,
-  predictedCGPA: Number
-});
-
-module.exports = mongoose.model("Performance", performanceSchema);
--- backend/routes/studentRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const Student = require("../models/Student");
-
-router.get("/:email", async (req, res) => {
-  const student = await Student.findOne({ email: req.params.email });
-  res.json(student);
-});
-
-module.exports = router;
